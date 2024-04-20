18ac1b572d7624eb56e318e0d9ed2e0fd8fb39f7
Paulius Preikša <preiksap@gmail.com>
2024-04-20 19:33:52 +0300

    feat: register toast

--- portal-frontend/src/services/api.jsx
+++ portal-frontend/src/services/api.jsx
@@ -26,7 +26,7 @@ export async function register(user) {
       },
     });
     console.log(response.data);
-
+    toastService.success('Registracija sėkminga! Prisijunkite!');
     return response.data;
   } catch (error) {
     console.error('Error registering user:', error);
